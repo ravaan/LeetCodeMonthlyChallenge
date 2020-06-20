6fce281dfc6e384f7b85a9b54f85a5d6655e7dde
ravaan <arpit.agarwal181@gmail.com>
2020-06-20 18:08:26 +0530

    improve code

--- 06-20-PermutationSequence.java
+++ 06-20-PermutationSequence.java
@@ -1,40 +1,23 @@
 class Solution {
-    boolean used[];
-    int fact = 1;
-
     public String getPermutation(int n, int k) {
-        if (n == 1) return "1";
-        used = new boolean[n + 1];
-
-        for (int i = 2; i < n; i++) fact *= i;
-        StringBuilder ans = new StringBuilder();
-        used[0] = true;
-        boolean flag = false;
-        while (k >= 1) {
-            // System.out.println("n: " + n + " k: " + k + " fact: " + fact);
-            int sm = k / fact;
-            if (k % fact != 0) sm++;
-            ans.append(findSmallest(sm));
-            n--;
-            if (k == 1) flag = true;
-            k -= (sm - 1) * fact;
-            if (flag) break;
-            fact /= n;
+        List<Integer> nums = new ArrayList();
+        nums.add(1);
+        long fact = 1;
+        for (int i = 1; i < n; ++i) {
+            fact *= i;
+            nums.add(i + 1);
         }
-        for (int i = 1; i < used.length; i++) if (!used[i]) ans.append(i);
-        return ans.toString();
-    }
+        k--;
+        StringBuilder sb = new StringBuilder();
+        for (int i = n - 1; i >= 0; i--) {
+            int idx = (int) (k / fact);
+            k -= idx * fact;
 
-    public int findSmallest(int s) {
-        int count = 0;
-        for (int i = 0; i < used.length; i++) {
-            if (!used[i]) count++;
-            if (count == s) {
-                used[i] = true;
-                return i;
-            }
+            sb.append(nums.get(idx));
+            nums.remove(idx);
+            if (i != 0) fact /= i;
         }
-        return 0;
+        return sb.toString();
     }
 }
 
